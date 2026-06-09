e9010b608ffa714d5eae3d637fbed4335962bdc4
tasyasiregar094 <tasyaaprianisiregar@gmail.com>
2026-06-09 12:39:56 +0700

    Update 02_string_kalimat.R

--- src_R/02_string_kalimat.R
+++ src_R/02_string_kalimat.R
@@ -1,7 +1,10 @@
 #SOAL 2
 K1 <- "Saya tak 'kan menyerah."
+
 K2 <- "Ia berkata, \"Aku menyayangimu.\""
+
 K3 <- "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
+
 K4 <- "Surat keputusan itu bernomor 62/UN.34/19/2023."
 
 cat(K1, "\n")
