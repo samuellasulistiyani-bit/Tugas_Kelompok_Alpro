1102dd9ce665c779388fba868a5a923d8924fd08
fachrirafakh <fachri.khaerulloh@mhs.unsoed.ac.id>
2026-06-10 06:26:01 +0700

    Create 06_interval_proporsi.py

---
+++ src_python/06_interval_proporsi.py
@@ -0,0 +1,21 @@
+def interval_kepercayaan(p_hat, n, alpha):
+    # Validasi proporsi
+    if p_hat < 0 or p_hat > 1:
+        return "Error: Proporsi harus berada antara 0 dan 1."
+
+    # Menentukan nilai z
+    if alpha == 0.10:
+        z = 1.645
+    elif alpha == 0.05:
+        z = 1.96
+    else:
+        return "Error: Alpha harus 0.10 atau 0.05."
+
+    # Menghitung margin of error
+    moe = sqrt((p_hat * (1 - p_hat)) / n)
+
+    # Batas bawah dan atas
+    lower = p_hat - (z*moe)
+    upper = p_hat + (z*moe)
+
+    print("interval kepercayaan :", (lower, upper))
