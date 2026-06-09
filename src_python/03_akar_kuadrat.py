1df4e60080b8f53d815f59b53563dc258441a2e8
samuellasulistiyani-bit <samuellasulistiyani@gmail.com>
2026-06-09 12:06:13 +0700

    Create 03_akar_kuadrat.py

---
+++ src_python/03_akar_kuadrat.py
@@ -0,0 +1,22 @@
+import math
+
+def akar_persamaan_kuadrat(a, b, c):
+    D = b**2 - 4*a*c
+
+    if D < 0:
+        print("Persamaan memiliki akar-akar imajiner.")
+    else:
+        x1 = (-b + math.sqrt(D)) / (2*a)
+        x2 = (-b - math.sqrt(D)) / (2*a)
+
+        print(f"x1 = {x1:.3f}")
+        print(f"x2 = {x2:.3f}")
+
+# contoh 1
+  akar_persamaan_kuadrat(1, -5, 6)
+# contoh 2
+  akar_persamaan_kuadrat(1, -4, 4)
+# contoh 3
+  akar_persamaan_kuadrat(1, 4, 5)
+# contoh 4
+  akar_persamaan_kuadrat(2, -5, 1)
