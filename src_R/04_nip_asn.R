3fcdba27fcd3165b4f7f3f7a166771eba8ace399
faisaldilla <faisal.dilla@mhs.unsoed.ac.id>
2026-06-09 12:07:35 +0700

    Create 04_nip_asn.R

---
+++ src_R/04_nip_asn.R
@@ -0,0 +1,40 @@
+# Program 4
+cek_nip <- function(nip) {
+  tahun <- substr(nip, 1, 4)
+  bulan <- substr(nip, 5, 6)
+  tanggal <- substr(nip, 7, 8)
+  
+  if (bulan == "01") {
+    nama_bulan <- "Januari"
+  } else if (bulan == "02") {
+    nama_bulan <- "Februari"
+  } else if (bulan == "03") {
+    nama_bulan <- "Maret"
+  } else if (bulan == "04") {
+    nama_bulan <- "April"
+  } else if (bulan == "05") {
+    nama_bulan <- "Mei"
+  } else if (bulan == "06") {
+    nama_bulan <- "Juni"
+  } else if (bulan == "07") {
+    nama_bulan <- "Juli"
+  } else if (bulan == "08") {
+    nama_bulan <- "Agustus"
+  } else if (bulan == "09") {
+    nama_bulan <- "September"
+  } else if (bulan == "10") {
+    nama_bulan <- "Oktober"
+  } else if (bulan == "11") {
+    nama_bulan <- "November"
+  } else if (bulan == "12") {
+    nama_bulan <- "Desember"
+  } else {
+    nama_bulan <- "Bulan tidak valid"
+  }
+  
+  cat("Tanggal Lahir ASN:", tanggal, nama_bulan, tahun,"\n")
+}
+
+cek_nip("199301212019031010")
+cek_nip("199812252019098768")
+cek_nip("199904150189087625")
