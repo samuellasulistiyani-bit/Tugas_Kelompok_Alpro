23c033129e89ef1961f8e7cba308e7f5c0426467
faisaldilla <faisal.dilla@mhs.unsoed.ac.id>
2026-06-09 13:13:50 +0700

    Update 05_cluster_3d.R

--- src_R/05_cluster_3d.R
+++ src_R/05_cluster_3d.R
@@ -1,21 +1,17 @@
 # Program 5
-jarak <- function(u, p) {
-  sqrt((u[1]-p[1])^2 + (u[2]-p[2])^2 + (u[3]-p[3])^2)
-}
-
 klasifikasi <- function(x1, x2, x3) {
   A <- c(2, 1, 3)
   B <- c(1, -4, 6)
   C <- c(-2, 3, -2)
   u <- c(x1, x2, x3)
   
-  jarak_A <- jarak(u, A)
-  jarak_B <- jarak(u, B)
-  jarak_C <- jarak(u, C)
+  dA <- sqrt((u[1]-A[1])^2 + (u[2]-A[2])^2 + (u[3]-A[3])^2)
+  dB <- sqrt((u[1]-B[1])^2 + (u[2]-B[2])^2 + (u[3]-B[3])^2)
+  dC <- sqrt((u[1]-C[1])^2 + (u[2]-C[2])^2 + (u[3]-C[3])^2)
   
-  if (jarak_A < jarak_B && jarak_A < jarak_C) {
+  if (dA < dB && dA < dC) {
     cluster <- "A"
-  } else if (jarak_B < jarak_A && jarak_B < jarak_C) {
+  } else if (dB < dA && dB < dC) {
     cluster <- "B"
   } else {
     cluster <- "C"
