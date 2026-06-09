2e68748916a2c9b0989cfecd280c8d74a0a0d4cc
samuellasulistiyani-bit <samuellasulistiyani@gmail.com>
2026-06-09 12:39:22 +0700

    Update 01_analisis_teks.py

--- src_python/01_analisis_teks.py
+++ src_python/01_analisis_teks.py
@@ -1,3 +1,4 @@
+# SOAL 1
 # Program menghitung jumlah kata dan jumlah kalimat
 def hitung_teks(teks):
     if '.' not in teks:
@@ -9,27 +10,60 @@ def hitung_teks(teks):
         print(f"Teks tersebut memuat {jumlah_kalimat} kalimat dan {jumlah_kata} kata.")
 # Pengujian 1
 teks1 = """
-Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif.
+Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook,
+Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan
+sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini
+juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini
+telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk
+mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini
+dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha
+bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat
+promosi yang paling efektif.
 """
 hitung_teks(teks1)
 
 # Pengujian 2
 teks2 = """
-Media sosial saat ini menjadi bagian penting dalam kehidupan masyarakat. Banyak orang menggunakan media sosial untuk berkomunikasi, mencari informasi, dan berbagi pengalaman dengan orang lain. Selain itu, media sosial juga dimanfaatkan oleh pelaku usaha untuk memasarkan produk dan jasa mereka.
+Metode penelitian kuantitatif ada 2 (dua) macam yaitu metode eksperimen 
+dan metode survey. Metode eksperimen adalah metode penelitian yang digunakan 
+untuk mencari pengaruh treatment tertentu (perlakuan) dalam kondisi yang 
+terkontrol (laboratorium) (Sugiyono, 2013). Sedangkan menurut Cresswell 
+bahwa experimental research seeks to determine if a specific treatment influence an 
+outcome in a study. This impact is assessed by providing a specific treatment to one 
+group and withholding it form another group and then determining how groups score 
+on an outcome (Creswell, 2013).
 """
 
 hitung_teks(teks2)
 
 # Pengujian 3
 teks3 = """
-Perpustakaan merupakan sumber ilmu pengetahuan yang penting bagi mahasiswa. Di perpustakaan, mahasiswa dapat membaca berbagai buku, jurnal, dan artikel ilmiah untuk mendukung proses belajar mereka. Selain menyediakan bahan bacaan, perpustakaan juga menyediakan ruang yang nyaman untuk belajar dan berdiskusi. Oleh karena itu, perpustakaan tetap memiliki peran penting di era digital saat ini.
+Ada dua jenis analisis data yang secara 
+umum dipakai dalam proses penelitian yaitu analisis data kuantitatif dan analisis data 
+kualitatif. Pembagian kedua jenis analisis data penelitian tersebut didasarkan pada 
+pendekatan penelitian yang digunakannya yaitu pendekatan penelitian kuantitatif 
+atau pendekatan penelitian kualitatif. Bisa juga pembagian jenis analisis data 
+penelitian ini didasarkan pada jenis data yang di analisisnya, jika jenis datanya berupa 
+data kuantitatif maka teknik analisis datanya juga menggunakan teknik analisis 
+kuantitatif begitu juga sebaliknya jika datanya kualitatif maka teknik analisisnya juga 
+menggunakan teknik analisis data kualitatif. Kedua factor tersebutlah yang 
+menentukan teknik analisis yang akan digunakan oleh seorang peneliti untuk 
+menganalisis data yang dikumpulkan dari lapangan.
 """
 
 hitung_teks(teks3)
 
 # Pengujian 4
 teks4 = """
-Media sosial saat ini menjadi bagian penting dalam kehidupan masyarakat, banyak orang menggunakannya setiap hari untuk berkomunikasi dan mencari informasi
+Literasi digital mencakup kemampuan teknis dan kognitif 
+dalam menggunakan perangkat digital seperti komputer, smartphone, 
+atau internet untuk mengakses informasi, membuat konten, serta berkomunikasi 
+dan berkolaborasi secara online Selain kemampuan operasional, literasi digital 
+juga menekankan berpikir kritis, etika digital, keamanan data pribadi, dan kesadaran 
+akan jejak digital yang ditinggalkan saat beraktivitas di dunia maya 
+Dengan kata lain, literasi digital bukan sekadar bisa menggunakan teknologi, 
+tetapi juga mampu membuat keputusan yang tepat dan bertanggung jawab dalam konteks 
+digital
 """
 
 hitung_teks(teks4)
