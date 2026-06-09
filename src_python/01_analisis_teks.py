e9da663b1c5a75fd0cb8baefe9bd393dce386a20
samuellasulistiyani-bit <samuellasulistiyani@gmail.com>
2026-06-09 11:33:27 +0700

    Update 01_analisis_teks.py

--- src_python/01_analisis_teks.py
+++ src_python/01_analisis_teks.py
@@ -7,6 +7,12 @@ def hitung_teks(teks):
         jumlah_kata = len(teks.split())
 
         print(f"Teks tersebut memuat {jumlah_kalimat} kalimat dan {jumlah_kata} kata.")
+# contoh
+teks = """
+Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif.
+"""
+
+hitung_teks(teks)
 # contoh 1
   teks = """
 Media sosial saat ini menjadi bagian penting dalam kehidupan masyarakat. Banyak orang menggunakan media sosial untuk berkomunikasi, mencari informasi, dan berbagi pengalaman dengan orang lain. Selain itu, media sosial juga dimanfaatkan oleh pelaku usaha untuk memasarkan produk dan jasa mereka.
