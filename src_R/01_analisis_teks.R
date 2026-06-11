39c560e9a50a71710f3b30447280ea4779326aed
tasyasiregar094 <tasyaaprianisiregar@gmail.com>
2026-06-12 03:05:59 +0700

    Update 01_analisis_teks.R

--- src_R/01_analisis_teks.R
+++ src_R/01_analisis_teks.R
@@ -10,19 +10,7 @@ hitung_teks <- function(teks){
 }
 
 #Pengujian 1
-teks1 <- "Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook,
-Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan
-sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini
-juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini
-telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk
-mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini
-dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha
-bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat
-promosi yang paling efektif."
-hitung_teks(teks1)
-
-#Pengujian 2
-teks2 <- "Metode penelitian kuantitatif ada 2 (dua) macam yaitu metode eksperimen 
+teks1 <- "Metode penelitian kuantitatif ada 2 (dua) macam yaitu metode eksperimen 
 dan metode survey. Metode eksperimen adalah metode penelitian yang digunakan 
 untuk mencari pengaruh treatment tertentu (perlakuan) dalam kondisi yang 
 terkontrol (laboratorium) (Sugiyono, 2013). Sedangkan menurut Cresswell 
@@ -30,28 +18,19 @@ bahwa experimental research seeks to determine if a specific treatment influence
 outcome in a study. This impact is assessed by providing a specific treatment to one 
 group and withholding it form another group and then determining how groups score 
 on an outcome (Creswell, 2013)."
+hitung_teks(teks1)
+
+#Pengujian 2
+teks3 <-"Salah satu media digital yang cukup populer saat ini adalah media sosial, 
+yang diantaranya ada instagram, facebook, tiktok, dan lain sebagainya."
 hitung_teks(teks2)
 
 #Pengujian 3
-teks3 <-"Ada dua jenis analisis data yang secara 
-umum dipakai dalam proses penelitian yaitu analisis data kuantitatif dan analisis data 
-kualitatif. Pembagian kedua jenis analisis data penelitian tersebut didasarkan pada 
-pendekatan penelitian yang digunakannya yaitu pendekatan penelitian kuantitatif 
-atau pendekatan penelitian kualitatif. Bisa juga pembagian jenis analisis data 
-penelitian ini didasarkan pada jenis data yang di analisisnya, jika jenis datanya berupa 
-data kuantitatif maka teknik analisis datanya juga menggunakan teknik analisis 
-kuantitatif begitu juga sebaliknya jika datanya kualitatif maka teknik analisisnya juga 
-menggunakan teknik analisis data kualitatif. Kedua factor tersebutlah yang 
-menentukan teknik analisis yang akan digunakan oleh seorang peneliti untuk 
-menganalisis data yang dikumpulkan dari lapangan."
-hitung_teks(teks3)
-
-#Pengujian 4
-teks4 <- "Literasi digital mencakup kemampuan 
+teks3 <- "Literasi digital mencakup kemampuan 
 teknis dan kognitif dalam menggunakan perangkat digital seperti komputer, smartphone, atau internet 
 untuk mengakses informasi, membuat konten, serta berkomunikasi dan berkolaborasi secara online 
 Selain kemampuan operasional, literasi digital juga menekankan berpikir kritis, etika digital, 
 keamanan data pribadi, dan kesadaran akan jejak digital yang ditinggalkan saat beraktivitas 
 di dunia maya Dengan kata lain, literasi digital bukan sekadar bisa menggunakan teknologi, 
 tetapi juga mampu membuat keputusan yang tepat dan bertanggung jawab dalam konteks digital"
-hitung_teks(teks4)
+hitung_teks(teks3) 
