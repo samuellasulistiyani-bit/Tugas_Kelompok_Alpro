84a7b95ef8f2159eeb9ea858802614ab9c6e6378
samuellasulistiyani-bit <samuellasulistiyani@gmail.com>
2026-06-17 12:12:03 +0700

    Update 03_akar_kuadrat.R

--- src_R/03_akar_kuadrat.R
+++ src_R/03_akar_kuadrat.R
@@ -1,16 +1,23 @@
-#SOAL 3
-akar_persamaan_kuadrat <- function(a, b, c) {
-  D <- b^2 - 4*a*c
-  
-  if (D < 0) {
-    cat("Persamaan memiliki akar-akar imajiner.\n")
-  } else {
-    x1 <- (-b + sqrt(D)) / (2*a)
-    x2 <- (-b - sqrt(D)) / (2*a)
-    cat(sprintf("x1 = %.3f\n", x1))
-    cat(sprintf("x2 = %.3f\n", x2))
-  }
-}
+# PROGRAM 3
+import math
+
+def akar_persamaan_kuadrat(*args):
+    if len(args) != 3:
+        print("Input tidak valid: masukkan 3 koefisien (a, b, c).")
+        return
+
+    a, b, c = args
+
+    D = b**2 - 4*a*c
+
+    if D < 0:
+        print("Persamaan memiliki akar-akar imajiner.")
+    else:
+        x1 = (-b + math.sqrt(D)) / (2*a)
+        x2 = (-b - math.sqrt(D)) / (2*a)
+
+        print(f"x1 = {x1:.3f}")
+        print(f"x2 = {x2:.3f}")
 
 #Pengujian 1
 akar_persamaan_kuadrat(1,-5,6)
