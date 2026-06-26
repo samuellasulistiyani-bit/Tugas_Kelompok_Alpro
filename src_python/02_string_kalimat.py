f9651bdeb917d87d92ff38ceabdbc8681107bd66
samuellasulistiyani-bit <samuellasulistiyani@gmail.com>
2026-06-26 17:27:02 +0700

    Update 02_string_kalimat.py

--- src_python/02_string_kalimat.py
+++ src_python/02_string_kalimat.py
@@ -3,9 +3,10 @@ K1 = "Saya tak 'kan menyerah."
 
 K2 = 'Ia berkata, "Aku menyayangimu."'
 
-K3 = "\"Coba jelaskan pengertian \\cross-validation' dalam Machine Learning!\""
+K3 = "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
 
 K4 = "Surat keputusan itu bernomor 62/UN.34/19/2023."
+
 print(K1)
 print(K2)
 print(K3)
